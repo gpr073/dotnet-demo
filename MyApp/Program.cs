9c5c0fa57a16b0c3bb04bb797953740609bd485c
gpr073 <lonewolf41320@gmail.com>
2024-01-23 23:16:25 +0530

    Privilege de-escalation

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
         }
